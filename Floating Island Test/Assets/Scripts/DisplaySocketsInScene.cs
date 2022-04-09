47c0109f56a9267b0eef353d9664b5f614d3ccbc
iloste <34749898+iloste@users.noreply.github.com>
2022-04-09 15:49:06 +0100

    Updated display sockets script
    The display sockets script only shows the sockets of tile that have been selected in the editor.

--- Floating Island Test/Assets/Scripts/DisplaySocketsInScene.cs
+++ Floating Island Test/Assets/Scripts/DisplaySocketsInScene.cs
@@ -7,52 +7,90 @@ public class DisplaySocketsInScene : MonoBehaviour
 {
     [SerializeField] MCTile tile;
 
+    bool displaySockets;
+
     private void OnDrawGizmos()
     {
-        if (tile != null)
+        if (Selection.count == 1)
         {
-            Gizmos.color = Color.red;
-            Gizmos.DrawSphere(transform.position, 0.1f);
-            // Gizmos.DrawLine(transform.position, transform.position + transform.forward);
-            Vector3 direction = Vector3.zero;
+            if (Selection.activeGameObject == this.gameObject)
+            {
+                displaySockets = true;
+            }
+            else
+            {
+                displaySockets = false;
+            }
+        }
+        else if (Selection.count > 1)
+        {
+            for (int i = 0; i < Selection.gameObjects.Length; i++)
+            {
+                if (Selection.gameObjects[i] == this.gameObject)
+                {
+                    displaySockets = true;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            displaySockets = false;
+        }
 
-            if (tile.sockets[0].number >= 0)
-            {
-                direction = transform.position + transform.forward / 2;
-                DrawConnection(direction, tile.sockets[0]);
-                DrawDirection(transform.position, direction * .75f, Color.red);
-            }
-            if (tile.sockets[1].number >= 0)
-            {
-                direction = transform.position + transform.right / 2;
-                DrawConnection(direction, tile.sockets[1]);
-                DrawDirection(transform.position, direction * .75f, Color.blue);
-            }
-            if (tile.sockets[2].number >= 0)
-            {
-                direction = transform.position - transform.forward / 2;
-                DrawConnection(direction, tile.sockets[2]);
-                DrawDirection(transform.position, direction * .75f, Color.blue);
-            }
-            if (tile.sockets[3].number >= 0)
-            {
-                direction = transform.position - transform.right / 2;
-                DrawConnection(direction, tile.sockets[3]);
-                DrawDirection(transform.position, direction * .75f, Color.blue);
-            }
-            if (tile.sockets[4].number >= 0)
-            {
-                direction = transform.position + transform.up / 2;
-                DrawConnection(direction, tile.sockets[4]);
-                DrawDirection(transform.position, direction * .75f, Color.blue);
-            }
-            if (tile.sockets[5].number >= 0)
-            {
-                direction = transform.position - transform.up / 2;
-                DrawConnection(direction, tile.sockets[5]);
-                DrawDirection(transform.position, direction * .75f, Color.blue);
-            }
 
+        if (displaySockets)
+        {
+            if (tile != null)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(transform.position, 0.1f);
+                // Gizmos.DrawLine(transform.position, transform.position + transform.forward);
+                DisplaySockets();
+            }
+        }
+    }
+
+
+    private void DisplaySockets()
+    {
+        Vector3 direction = Vector3.zero;
+
+        if (tile.sockets[0].number >= 0)
+        {
+            direction = transform.position + transform.forward / 3;
+            DrawConnection(direction, tile.sockets[0]);
+            // DrawDirection(transform.position, direction * .75f, Color.red);
+        }
+        if (tile.sockets[1].number >= 0)
+        {
+            direction = transform.position + transform.right / 3;
+            DrawConnection(direction, tile.sockets[1]);
+            //  DrawDirection(transform.position, direction * .75f, Color.blue);
+        }
+        if (tile.sockets[2].number >= 0)
+        {
+            direction = transform.position - transform.forward / 3;
+            DrawConnection(direction, tile.sockets[2]);
+            // DrawDirection(transform.position, direction * .75f, Color.blue);
+        }
+        if (tile.sockets[3].number >= 0)
+        {
+            direction = transform.position - transform.right / 3;
+            DrawConnection(direction, tile.sockets[3]);
+            // DrawDirection(transform.position, direction * .75f, Color.blue);
+        }
+        if (tile.sockets[4].number >= 0)
+        {
+            direction = transform.position + transform.up / 3;
+            DrawConnection(direction, tile.sockets[4]);
+            // DrawDirection(transform.position, direction * .75f, Color.blue);
+        }
+        if (tile.sockets[5].number >= 0)
+        {
+            direction = transform.position - transform.up / 3;
+            DrawConnection(direction, tile.sockets[5]);
+            // DrawDirection(transform.position, direction * .75f, Color.blue);
         }
     }
 
